a2ac78ab7b1c0f078e909247b1e5a1c8a3827377
Mahrukh khan <107424325+Suga7772@users.noreply.github.com>
2023-08-04 14:00:42 +0500

    made changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<center><h1>changes to flask deployment on azure!</h1></center>'
+    return '<center><h1>hello  deployment on azure!</h1></center>'
 
 if __name__ == '__main__':
     app.run()
